e0b44f11a59a5c7a19b99bc8a969faab5c042e4c
Aurea <94511889+Aurea6@users.noreply.github.com>
2024-08-07 19:34:41 +0530

    Create main.js
    main file

---
+++ main.js
@@ -0,0 +1,104 @@
+const { ForgeClient } = require("@tryforge/forgescript")
+const { ForgeDB } = require("@tryforge/forge.db")
+   
+// Client initialization
+   const client = new ForgeClient({
+    "intents": [
+        "Guilds",
+        "GuildMembers",
+        "GuildModeration",
+        "GuildEmojisAndStickers",
+        "GuildIntegrations",
+        "GuildWebhooks",
+        "GuildInvites",
+        "GuildVoiceStates",
+        "GuildPresences",
+        "GuildMessages",
+        "GuildMessageReactions",
+        "GuildMessageTyping",
+        "DirectMessages",
+        "DirectMessageReactions",
+        "DirectMessageTyping",
+        "MessageContent",
+        "GuildScheduledEvents",
+        "AutoModerationConfiguration",
+        "AutoModerationExecution"
+    ],
+    "useInviteSystem": true,
+    "prefixes": [
+        "?"
+    ],
+    "events": [
+        "voiceStateUpdate",
+        "userUpdate",
+        "typingStart",
+        "threadUpdate",
+        "threadMemberUpdate",
+        "threadDelete",
+        "threadCreate",
+        "stickerUpdate",
+        "stickerDelete",
+        "stickerCreate",
+        "stageInstanceUpdate",
+        "stageInstanceDelete",
+        "stageInstanceCreate",
+        "shardResume",
+        "shardReconnecting",
+        "shardReady",
+        "shardError",
+        "shardDisconnect",
+        "roleUpdate",
+        "roleDelete",
+        "roleCreate",
+        "ready",
+        "presenceUpdate",
+        "messageUpdate",
+        "messageReactionRemoveEmoji",
+        "messageReactionRemoveAll",
+        "messageReactionRemove",
+        "messageReactionAdd",
+        "messageDeleteBulk",
+        "messageDelete",
+        "messageCreate",
+        "inviteDelete",
+        "inviteCreate",
+        "interactionCreate",
+        "guildUpdate",
+        "guildUnavailable",
+        "guildScheduledEventUserRemove",
+        "guildScheduledEventUserAdd",
+        "guildScheduledEventUpdate",
+        "guildScheduledEventDelete",
+        "guildScheduledEventCreate",
+        "guildMemberUpdate",
+        "guildMemberRemove",
+        "guildMemberAvailable",
+        "guildMemberAdd",
+        "guildDelete",
+        "guildCreate",
+        "guildBanRemove",
+        "guildBanAdd",
+        "guildAvailable",
+        "guildAuditLogEntryCreate",
+        "error",
+        "emojiUpdate",
+        "emojiDelete",
+        "emojiCreate",
+        "debug",
+        "channelUpdate",
+        "channelPinsUpdate",
+        "channelDelete",
+        "channelCreate",
+        "autoModerationActionExecution"
+    ],
+    "extensions": [
+        new ForgeDB()
+    ]
+})
+   
+// Load the commands
+   client.commands.load("cmd")
+   client.applicationCommands.load("sls")
+   
+// Your bot token
+   client.login("Your bot token here");
