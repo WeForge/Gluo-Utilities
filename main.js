0742980663be7e40da25f054ea665d3ba6faa2d1
Aurea <94511889+Aurea6@users.noreply.github.com>
2024-08-08 21:45:01 +0530

    Update main.js
    removed invite system stuff because not needed

--- main.js
+++ main.js
@@ -24,7 +24,6 @@ const { ForgeDB } = require("@tryforge/forge.db")
         "AutoModerationConfiguration",
         "AutoModerationExecution"
     ],
-    "useInviteSystem": true,
     "prefixes": [
         "?"
     ],
