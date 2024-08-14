9cecda6edd96ac44a99ccf6260576463241e812f
Aurea <94511889+Aurea6@users.noreply.github.com>
2024-08-14 17:36:12 +0530

    Update interactions.js
    what i think is called a fix

--- cmds/interactions.js
+++ cmds/interactions.js
@@ -4,6 +4,6 @@ module.exports = {
   code: `
   $if[$customID==fixed;
     $deleteChannels[$findChannel[query;true]]
-    $sendMessage[1248555821353402449;$title[$option[severity] severity bug is fixed] $description[Description of bug:\n> $option[description]] $footer[Bug report closed by $username[$authorID];$authorAvatar];false]
+    $sendMessage[1248555821353402449;$title[$option[severity] severity bug is fixed] $description[Description of bug:\n> $option[description]] $footer[Bug report closed by $username[$authorID];$userAvatar[$authorID]];false]
   ]`
 }
