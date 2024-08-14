974caa9d6508de686f8dfd1be3117317a0c66393
Aurea <94511889+Aurea6@users.noreply.github.com>
2024-08-14 17:27:15 +0530

    Update interactions.js
    testing part three of logs

--- cmds/interactions.js
+++ cmds/interactions.js
@@ -1,7 +1,10 @@
+/* logs part three */
 module.exports = {
   type: "interactionCreate",
   code: `
   $if[$customID==fixed;
-    $findChannel[query;true]
+    $let[x;$findChannel[query;true]]
+    $deleteChannels[x]
+    $sendMessage[1248555821353402449;$title[$option[severity] severity bug is fixed] $description[Description of bug:\n> $option[description]] $footer[Bug report closed by $username[$authorID];$authorAvatar];false]
   ]`
 }
