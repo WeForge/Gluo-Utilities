8f843c5503ad4378ec0903348351c9e5c2d4d3cb
Aurea <94511889+Aurea6@users.noreply.github.com>
2024-08-14 17:30:27 +0530

    Update interactions.js
    trying option two

--- cmds/interactions.js
+++ cmds/interactions.js
@@ -3,8 +3,7 @@ module.exports = {
   type: "interactionCreate",
   code: `
   $if[$customID==fixed;
-    $let[x;$findChannel[query;true]]
-    $deleteChannels[x]
+    $deleteChannels[$findChannel[query;true]]
     $sendMessage[1248555821353402449;$title[$option[severity] severity bug is fixed] $description[Description of bug:\n> $option[description]] $footer[Bug report closed by $username[$authorID];$authorAvatar];false]
   ]`
 }
