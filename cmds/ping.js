ea492ed7232c1442d81ed248ee3174e5ada78be7
Aurea <94511889+Aurea6@users.noreply.github.com>
2024-08-16 16:23:06 +0530

    Update ping.js

--- cmds/ping.js
+++ cmds/ping.js
@@ -1,6 +1,1 @@
-module.exports={
-  name: "ping",
-  aliases: ["pong"],
-  type: "messageCreate",
-  code: `$round[$ping;2]ms`
-}
+module.exports={name: "ping",aliases: ["pong"],type: "messageCreate",code: `$round[$ping;2]ms`}
