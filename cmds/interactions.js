f6b73c6fbfe105d01c0be42ca167888e76fd3dbf
Aurea <94511889+Aurea6@users.noreply.github.com>
2024-08-16 16:22:42 +0530

    Update interactions.js
    converting stuff into one liners

--- cmds/interactions.js
+++ cmds/interactions.js
@@ -1,9 +1,1 @@
-/* logs part three */
-module.exports = {
-  type: "interactionCreate",
-  code: `
-  $if[$customID==fixed;
-    $deleteChannels[$findChannel[query;true]]
-    $sendMessage[1248555821353402449;$title[$option[severity] severity bug is fixed] $description[Description of bug:\n> $option[description]] $footer[Bug report closed by $username[$authorID];$userAvatar[$authorID]];false]
-  ]`
-}
+module.exports={type: "interactionCreate",code: `$if[$customID==fixed;$deleteChannels[$findChannel[query;true]]$sendMessage[1248555821353402449;$title[$option[severity] severity bug is fixed] $description[Description of bug:\n> $option[description]] $footer[Bug report closed by $username[$authorID];$userAvatar[$authorID]];false]]`}
