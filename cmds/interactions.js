7db20cdd0934fa91488829f231323d4a4961272f
Aurea <94511889+Aurea6@users.noreply.github.com>
2024-08-18 11:27:48 +0530

    Update interactions.js

--- cmds/interactions.js
+++ cmds/interactions.js
@@ -1,1 +1,6 @@
-module.exports={type: "interactionCreate",code: `$if[$customID==fixed;$deleteChannels[$findChannel[query;true]]$sendMessage[$getGuildVar[channelID_of_logs]];$title[$getGlobalVar[rps-$getGuildVar[r;$guildID]] severity bug is fixed] $description[Description of bug:\n> $cropText[$getGlobalVar[rpd-$getGuildVar[r;$guildID]]];0;160;…] $footer[Bug report closed by $username[$authorID];$userAvatar[$authorID]];false]]`}
+module.exports={type: "interactionCreate",code: `$if[$customID==fixed;
+$deleteChannels[$findChannel[query;true]]$sendMessage[$getGuildVar[channelID_of_logs]];
+$title[$getGlobalVar[rps-$getGuildVar[r;$guildID]] severity bug is fixed] 
+$description[Description of bug:\n> $cropText[$getGlobalVar[rpd-$getGuildVar[r;$guildID]]];0;160;…]
+$footer[Bug report closed by $username[$authorID];$userAvatar[$authorID]]
+;false]]`}
