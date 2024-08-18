54877f6bc97e75a4d1f036a37d091dfa4f7ecd96
Aurea <94511889+Aurea6@users.noreply.github.com>
2024-08-18 16:45:34 +0530

    Add files via upload

--- sls/bug-report.js
+++ sls/bug-report.js
@@ -31,9 +31,12 @@ module.exports={
       }
     ]
   },
-  code: `$defer
+  code: `
+
   $let[b;$createChannel[$guildID;bug-$option[severity]-$getGuildVar[a];GuildText;Please wait, developers will take sometime to respond.;$getGuildVar[categoryID_in_which_reports_will_be_made]]]
-  $ephemeral $author[Kipteam;$userAvatar[675316333780533268]] $description[### Thank you, $userDisplayName[$authorID], for creating a report.\n> -# **Our developers** will be in contact with you shortly. In the meantime, please provide us with **additional details** on the bug in **<#$get[b]>** while you wait.]
+
+$ephemeral ### Thank you, $userDisplayName[$authorID], for creating a report.\n> -# **Our developers** will be in contact with you shortly. In the meantime, please provide us with **additional details** on the bug in **<#$get[b]>** while you wait. 
+
   $!addChannelPerms[$get[b];$authorID;AddReactions;ViewChannel;SendMessages;EmbedLinks;AttachFiles;ReadMessageHistory] 
   $sendMessage[$get[b];<@$authorID> $author[Kipteam;$userAvatar[675316333780533268]] $description[### Thanks for creating this bug report.
   > -# **Our developers** will contact you shortly. In the meantime, please give us **all the details** on the bug. You can also just provide answers to these questions:
@@ -44,7 +47,7 @@ module.exports={
   - What OS do you use? Provide it's version.
   - Can you send screenshots, videos, etc.?]
   $addActionRow
-  $addButton[fixed;Bug is fixed;Primary;;false];false]
+  $addButton[fixed;Bug is fixed;Success;;false];false]
   $sendMessage[$getGuildVar[channelID_of_logs];New bug report is made at https://discord.com/channels/$guildID/$get[b]
   $title[$option[severity] severity bug] $description[Description of bug:\n> $cropText[$option[description];0;160;…]] $footer[Bug report created by $username[$authorID];$userAvatar[$authorID]]
   ;false]
