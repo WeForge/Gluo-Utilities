d66aa811cd796a251f701d57dfb0a2c2c47c618d
Aurea <94511889+Aurea6@users.noreply.github.com>
2024-08-18 16:20:33 +0530

    Update bug-report.js

--- sls/bug-report.js
+++ sls/bug-report.js
@@ -32,10 +32,8 @@ module.exports={
     ]
   },
   code: `$defer
-  $setGuildVar[a;$sum[$getGuildVar[a];1]] $c[increment id]
-  $let[a;675316333780533268]
   $let[b;$createChannel[$guildID;bug-$option[severity]-$getGuildVar[a];GuildText;Please wait, developers will take sometime to respond.;$getGuildVar[categoryID_in_which_reports_will_be_made]]]
-  $ephemeral $author[Kipteam;$userAvatar[$get[a];2048;webp]] $description[### Thank you, $userDisplayName[$authorID], for creating a report.\n> -# **Our developers** will be in contact with you shortly. In the meantime, please provide us with **additional details** on the bug in **<#$get[b]>** while you wait.]
+  $ephemeral $author[Kipteam;$userAvatar[675316333780533268]] $description[### Thank you, $userDisplayName[$authorID], for creating a report.\n> -# **Our developers** will be in contact with you shortly. In the meantime, please provide us with **additional details** on the bug in **<#$get[b]>** while you wait.]
   $!addChannelPerms[$get[b];$authorID;AddReactions;ViewChannel;SendMessages;EmbedLinks;AttachFiles;ReadMessageHistory] 
   $sendMessage[$get[b];<@$authorID> $author[Kipteam;$userAvatar[$get[a];2048;webp]] $description[### Thanks for creating this bug report.
   > -# **Our developers** will contact you shortly. In the meantime, please give us **all the details** on the bug. You can also just provide answers to these questions:
@@ -46,11 +44,12 @@ module.exports={
   - What OS do you use? Provide it's version.
   - Can you send screenshots, videos, etc.?]
   $addActionRow
-$addButton[fixed;Bug is fixed;Primary;;false];false]
-$sendMessage[$getGuildVar[channelID_of_logs];New bug report is made at https://discord.com/channels/$guildID/$get[b]
+  $addButton[fixed;Bug is fixed;Primary;;false];false]
+  $sendMessage[$getGuildVar[channelID_of_logs];New bug report is made at https://discord.com/channels/$guildID/$get[b]
   $title[$option[severity] severity bug] $description[Description of bug:\n> $cropText[$option[description];0;160;…]] $footer[Bug report created by $username[$authorID];$userAvatar[$authorID]]
   ;false]
   $setGuildVar[severity-$authorID-$get[b];$option[severity]]
   $setGuildVar[description-$authorID-$get[b];$option[description]]
- `
+  $setGuildVar[a;$sum[$getGuildVar[a];1]]
+  `
 }
