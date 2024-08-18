6a2ad08be68f08d0e835e7e8f4458841b9e02f53
Aurea <94511889+Aurea6@users.noreply.github.com>
2024-08-18 16:06:26 +0530

    Update interactions.js

--- cmds/interactions.js
+++ cmds/interactions.js
@@ -1,10 +1,10 @@
-module.exports={type: "interactionCreate",code: `$if[$customID==fixed;
-$deleteChannels[$findChannel[query;true]]$sendMessage[$getGuildVar[channelID_of_logs];
-$title[$getGlobalVar[rps-$getGuildVar[$getGuildVar[a]-$authorID]] severity bug is fixed] 
-$description[Description of bug:\n> $cropText[$getGlobalVar[rpd-$getGuildVar[$getGuildVar[a]-$authorID]];0;160;…]]
-$footer[Bug report closed by $username[$authorID];$userAvatar[$authorID]]
-;false]] `}
-/* $if[$customID==bug-$getGuildVar[a]-$authorID;
-$editMessage[$channelID;$messageID;$title[$getGlobalVar[rps-$getGuildVar[r;$guildID]] severity bug is fixed]
-$description[Description of bug:\n> $cropText[$getGlobalVar[rpd-$getGuildVar[r;$guildID]];0;160;…]]
-$footer[Bug report closed by $username[$authorID];$userAvatar[$authorID]]];$ephemeral Sorry, but only the person who reported this bug can mark it as fixed.]*/
+module.exports={
+  type: "interactionCreate",
+  code: `$if[$customID==fixed;
+  $deleteChannels[$channelID]$sendMessage[$getGuildVar[channelID_of_logs];
+  $title[$getGuildVar[severity-$authorID-$channelID] severity bug is fixed] 
+  $description[Description of bug:\n> $cropText[$getGuildVar[description-$authorID-$channelID];0;160;…]]
+  $footer[Bug report closed by $username[$authorID];$userAvatar[$authorID]]
+  ;false]]
+  `
+}
