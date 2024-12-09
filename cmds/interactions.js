a2ac739f0647fddd55db143a68abac03a89808d8
Aurea <94511889+Aurea6@users.noreply.github.com>
2024-12-09 16:03:53 +0530

    Update interactions.js

--- cmds/interactions.js
+++ cmds/interactions.js
@@ -2,8 +2,8 @@ module.exports={
   type: "interactionCreate",
   code: `$if[$customID==fixed;
   $deleteChannels[$channelID]$sendMessage[$getGuildVar[channelID_of_logs];
-  $title[$getGuildVar[severity-$authorID-$channelID] severity bug is fixed] 
-  $description[Description of bug:\n> $cropText[$getGuildVar[description-$authorID-$channelID];0;160;…]]
+  $title[$getGuildVar[severity-$channelID] severity bug is fixed] 
+  $description[Description of bug:\n> $cropText[$getGuildVar[description-$channelID];0;160;…]]
   $footer[Bug report closed by $username[$authorID];$userAvatar[$authorID]]
   ;false]]
   `
