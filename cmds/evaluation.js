cf89f7ab8ef3eb34a2160168d350fdc48590c860
Aurea <94511889+Aurea6@users.noreply.github.com>
2025-06-13 21:32:44 +0530

    Add files via upload

---
+++ cmds/evaluation.js
@@ -0,0 +1,8 @@
+module.exports={
+  name: "evaluation",
+  aliases: ["e", "eval"],
+  type: "messageCreate",
+  code: `
+  $onlyForUsers[Sorry, but only **authorized** users can use this command :/;976413539076026388;675316333780533268]
+  `
+}
