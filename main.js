7f0f02258d7cc8a3bfc84a93ffbfb9d0c93be647
Aurea <94511889+Aurea6@users.noreply.github.com>
2025-06-13 21:12:09 +0530

    added db
    using new db, ditching aggeloss' forgedb

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-/*loading stuff up*/const { ForgeClient, LogPriority } = require("@tryforge/forgescript");
-/*initializing and setting up client*/const client = new ForgeClient({intents: ["GuildMessages"], events: ["messageCreate", "interactionCreate"], prefixes: ["."], logLevel: LogPriority.High, allowBots: true, mobile: true, respondOnEdit: true});
+/*loading stuff up*/const { ForgeClient, LogPriority } = require("@tryforge/forgescript"); const { QuorielDB } = require("@quoriel/db");
+/*initializing and setting up client*/const client = new ForgeClient({intents: ["GuildMessages"], events: ["messageCreate", "interactionCreate"], prefixes: ["."], logLevel: LogPriority.High, allowBots: true, mobile: true, respondOnEdit: true, extensions: [new QuorielDB]});
 /*loading stuff ane logging into client*/client.commands.load('legacy');client.applicationCommands.load('app');client.login(process.env.TOKEN);
