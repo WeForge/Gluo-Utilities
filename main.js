4cb32d8469ceb6e973808c7b19dbc4fa85befcd0
Aurea <94511889+Aurea6@users.noreply.github.com>
2025-06-17 12:43:16 +0530

    Update main.js
    using forgedb again, lazy to fix errors of quorealdb rn and it's in russian rn.

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-/*loading stuff up*/const { ForgeClient, LogPriority } = require("@tryforge/forgescript"); const { QuorielDB } = require("@quoriel/db");
-/*initializing and setting up client*/const client = new ForgeClient({intents: ["GuildMessages"], events: ["messageCreate", "interactionCreate"], prefixes: ["."], logLevel: LogPriority.High, allowBots: true, mobile: true, respondOnEdit: true, extensions: [new QuorielDB]});
+/*loading stuff up*/const { ForgeClient, LogPriority } = require("@tryforge/forgescript"); const { ForgeDB } = require("@tryforge/forge.db")
+/*initializing and setting up client*/const client = new ForgeClient({intents: ["GuildMessages"], events: ["messageCreate", "interactionCreate"], prefixes: ["."], logLevel: LogPriority.High, allowBots: false, mobile: true, respondOnEdit: true, extensions: [new ForgeDB()]});
 /*loading stuff ane logging into client*/client.commands.load('legacy');client.applicationCommands.load('app');client.login(process.env.TOKEN);
