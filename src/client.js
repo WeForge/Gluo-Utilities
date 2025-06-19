c37c746252fde20cd5babdab231cbfb21641ad9e
Aurea <94511889+Aurea6@users.noreply.github.com>
2025-06-19 09:52:16 +0530

    Update client.js

--- src/client.js
+++ src/client.js
@@ -1,12 +1,10 @@
-const { ForgeClient, LogPriority } = require("@tryforge/forgescript");
-const { QuorielDB } = require("@quoriel/db");
-const a = new ForgeClient({
-  intents: ["GuildMessages"],
-  events: ["messageCreate", "interactionCreate"],
-  prefixes: ["."],
-  logLevel: LogPriority.High,
-  mobile: true,
-  respondOnEdit: true,
-  extensions: [new QuorielDB()],
-});
+const{ForgeClient,LogPriority}=require("@tryforge/forgescript"),{QuorielDB}=require("@quoriel/db");
+const a=new ForgeClient({
+  intents:["GuildMessages"],
+  events:["messageCreate","interactionCreate"],
+  prefixes:["."],
+  logLevel:LogPriority.High,
+  mobile:1,
+  respondOnEdit:1,
+  extensions:[new QuorielDB()]});
 module.exports = a;
