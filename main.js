2ae6270b8343c4e04930f00117112b1ea08be483
Aurea <94511889+Aurea6@users.noreply.github.com>
2025-06-19 09:34:00 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,1 @@
-const { ForgeClient, LogPriority } = require("@tryforge/forgescript"); const { QuorielDB } = require("@quoriel/db"); require('dotenv').config(); // loads stuff up
-const client = new ForgeClient({intents: ["GuildMessages"], events: ["messageCreate", "interactionCreate"], prefixes: ["."],
-                                logLevel: LogPriority.High, mobile: true, respondOnEdit: true, extensions: [new QuorielDB()]}); // initializing client and setting stuff up
-client.commands.load('legacy');client.applicationCommands.load('app');client.login(process.env.TOKEN); // loading commands and logging into client
+require('./loader');
