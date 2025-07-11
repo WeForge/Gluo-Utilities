322d6c8e8bd3a11478b9ba610c07331af62f1e31
Aurea <94511889+Aurea6@users.noreply.github.com>
2025-07-11 18:50:45 +0530

    Update loader.js

--- src/loader.js
+++ src/loader.js
@@ -1,4 +1,4 @@
 require('dotenv').config({ path: '../.env' }); const agg = require('./client');
 agg.commands.load('/commands/legacy');
 agg.applicationCommands.load('../commands/app');
-agg.login(process.env.TOKEN);
+agg.login(process.env.your_mom);
